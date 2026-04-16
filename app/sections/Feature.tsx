c1cdac16b73c7b4ce1d821b7d9d176910361ed8a
Karen Agudelo Villa <agudelok838@gmail.com>
2026-04-16 00:15:32 -0500

    feat(BenefitsCard): new componenet implementation

--- app/sections/Feature.tsx
+++ app/sections/Feature.tsx
@@ -59,12 +59,16 @@ export default function Feature (){
             <CardUsage numeration="3" title="AI executes" items={runTools}  />
         </div>
 
-        <div className=" flex  flex-wrap gap-20 justify-center " >
-            <BenefictsCard icon = '/bolt.svg' title="Velocity" description="Accelerate execution by turning structured workflows into automated actions that move instantly from planning to delivery." />
-            <BenefictsCard icon = '/cube.svg' title="Clarity" description="Eliminate ambiguity across your operations by keeping every decision, input, and process aligned in a single intelligent system." />
-            <BenefictsCard icon = '/ring-.svg' title="Continuity" description="Maintain seamless context across all tasks and teams, ensuring nothing is lost between steps, tools, or handoffs." /> 
-            <BenefictsCard icon = '/chart.svg' title="Scale" description="Increase efficiency by reducing manual intervention, minimizing errors, and ensuring consistent execution of tasks." /> 
+        <div className="mt-12 sm:mt-16 md:mt-40 flex flex-col items-center gap-10">
+            <TextDescription title="The Compound Effect of Unified Intelligence" styles="text-center whitespace-normal break-words leading-tight px-4  max-w-4xl mx-auto"></TextDescription>
+            <div className=" flex  flex-wrap gap-20 justify-center " >
+                <BenefictsCard icon = '/bolt.svg' title="Velocity" description="Accelerate execution by turning structured workflows into automated actions that move instantly from planning to delivery." />
+                <BenefictsCard icon = '/cube.svg' title="Clarity" description="Eliminate ambiguity across your operations by keeping every decision, input, and process aligned in a single intelligent system." />
+                <BenefictsCard icon = '/ring-.svg' title="Continuity" description="Maintain seamless context across all tasks and teams, ensuring nothing is lost between steps, tools, or handoffs." /> 
+                <BenefictsCard icon = '/chart.svg' title="Scale" description="Increase efficiency by reducing manual intervention, minimizing errors, and ensuring consistent execution of tasks." /> 
+            </div>
         </div>
+
         </>
     )
 }
