e42e13014c7db991e71accff41071fd590b4a122
Karen Agudelo Villa <agudelok838@gmail.com>
2026-04-16 12:13:03 -0500

    Feat(Reviews): styles fix

--- app/sections/Review.tsx
+++ app/sections/Review.tsx
@@ -4,31 +4,37 @@ import TextDescription from "../components/TextDescription";
 export default function Review() {
   return (
     <>
-    <div className="flex md:mt-16 flex-col md:flex-row items-center justify-center gap-12 md:gap-20"> 
-
-        <ReviewCard
-            quote="Before this, our workflows were scattered across multiple tools and barely documented. Now everything is centralized, visible, and actually understandable by the entire team."
-            name="Slava"
-            title="CEO at Oxi Trade"
-            avatarId={32} 
-        />
-
-        
-        <ReviewCard
-        quote="What impressed us most is how quickly an idea turns into a functional system. The platform generated the full workflow—including integrations"
-        name="Karen"
-        title="Moxie Product Manager"
-        avatarId={35} 
+      <div className="md:mt-20 mt-10">
+      <TextDescription
+        title="Trusted by Teams Worldwide"
+        styles=" text-center max-w-2xl mx-auto p-4"
+        description="Discover how FlowPilot has transformed workflows and boosted efficiency."
       />
 
-        <ReviewCard
-        quote="We replaced three automation tools and cut our operational costs by 42% in the first month. Everything is now centralized, faster, and easier to maintain."
-        name="Damian"
-        title="CTO, Fintech Startup"
-        avatarId={8} 
-      />
+      </div>
+        <div className="flex md:mt-16 flex-col md:flex-row items-center justify-center gap-12 md:gap-20"> 
+            <ReviewCard
+                quote="Before this, our workflows were scattered across multiple tools and barely documented. Now everything is centralized, visible, and actually understandable by the entire team."
+                name="Slava"
+                title="CEO at Oxi Trade"
+                avatarId={32} 
+            />
 
-    </div>
+            
+            <ReviewCard
+            quote="What impressed us most is how quickly an idea turns into a functional system. The platform generated the full workflow—including integrations"
+            name="Karen"
+            title="Moxie Product Manager"
+            avatarId={35} 
+          />
 
+            <ReviewCard
+            quote="We replaced three automation tools and cut our operational costs by 42% in the first month. Everything is now centralized, faster, and easier to maintain."
+            name="Damian"
+            title="CTO, Fintech Startup"
+            avatarId={8} 
+          />
+
+        </div>
     </>
     )}
